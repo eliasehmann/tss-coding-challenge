29f12f7c7d581c791eee10970c394165db60ab4f
Elias Ehmann <elias.ehmann@gmail.com>
2021-07-15 19:51:53 +0200

    added Solution and Projects #1

---
+++ src/TSSCodingChallenge.IntervalMerge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TSSCodingChallenge.IntervalMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
