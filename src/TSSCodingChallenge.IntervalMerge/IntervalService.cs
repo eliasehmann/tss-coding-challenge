f66c34b39576b9bfa57c3c799f12770d93f0f4b3
Elias Ehmann <eehmann@spirit21.com>
2021-07-17 16:22:54 +0200

    added validation and parsing code # 4

--- src/TSSCodingChallenge.IntervalMerge/IntervalService.cs
+++ src/TSSCodingChallenge.IntervalMerge/IntervalService.cs
@@ -18,7 +18,48 @@ namespace TSSCodingChallenge.IntervalMerge
 
         public static List<Tuple<int, int>> ValidateAndParseIntervalsString(string intervalsString)
         {
-            throw new NotImplementedException();
+
+
+            // remove whitespaces
+            string intervalsStringNoSpaces = Regex.Replace(intervalsString, @"\s+", "");
+
+            var intervals = new List<Tuple<int, int>>();
+
+            // split into intervalString
+            foreach (Match intervalString in Regex.Matches(intervalsStringNoSpaces, @"\[(.+?)\]"))
+            {
+                try
+                {
+                    // The following code is not optimized! There's a better way to do it
+
+                    // remove brace [
+                    string intervalsStringNoLeft = Regex.Replace(intervalString.Value, @"\[+", "");
+                    // remove brace ]
+                    string intervalsStringNoRight = Regex.Replace(intervalsStringNoLeft, @"\]+", "");
+                    // split and convert into array
+                    string[] namesArray = intervalsStringNoRight.Split(',');
+
+                    int[] myInts = Array.ConvertAll(namesArray, s => int.Parse(s));
+
+                    var interval = new Tuple<int, int>(myInts[0], myInts[1]);
+
+                    intervals.Add(interval);
+                }
+                catch
+                {
+                    throw new Exception("No intervals found (Please check your input)");
+                }
+            }
+
+            if (intervals.Count == 0)
+            {
+                throw new Exception("No intervals found (Please check your input)");
+            }
+            Console.WriteLine(intervals.ToString());
+
+
+
+            return intervals;
         }
 
     }
