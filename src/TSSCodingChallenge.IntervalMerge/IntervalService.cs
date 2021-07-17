e1ff81e3763d9a3ad4b201cc7dcccc83d6e4295b
eliasehmann <eliaz.develops@gmail.com>
2021-07-17 15:54:08 +0200

    Added basic tests for checking input validity #3

--- src/TSSCodingChallenge.IntervalMerge/IntervalService.cs
+++ src/TSSCodingChallenge.IntervalMerge/IntervalService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TSSCodingChallenge.IntervalMerge
@@ -9,9 +10,16 @@ namespace TSSCodingChallenge.IntervalMerge
     public static class IntervalService
     {
 
-        public static void MergeIntervals()
-        {
 
+        public static List<Tuple<int, int>> MergeAndSortIntervals(List<Tuple<int, int>> intervals)
+        {
+            throw new NotImplementedException();
         }
+
+        public static List<Tuple<int, int>> ValidateAndParseIntervalsString(string intervalsString)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
