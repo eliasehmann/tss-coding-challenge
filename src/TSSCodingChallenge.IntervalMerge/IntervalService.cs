ed61e4e50f9bbe1acc1967d0873f81fded532f3d
Elias Ehmann <eehmann@spirit21.com>
2021-07-18 14:23:51 +0200

    added MergeAndSort #4

--- src/TSSCodingChallenge.IntervalMerge/IntervalService.cs
+++ src/TSSCodingChallenge.IntervalMerge/IntervalService.cs
@@ -13,13 +13,39 @@ namespace TSSCodingChallenge.IntervalMerge
 
         public static List<Tuple<int, int>> MergeAndSortIntervals(List<Tuple<int, int>> intervals)
         {
-            throw new NotImplementedException();
+
+
+            // Sort intervals by first item
+            intervals.Sort((x, y) => x.Item1.CompareTo(y.Item1));
+
+            var mergedIntervals = new List<Tuple<int, int>>();
+
+            for (int i = 0; i < intervals.Count; i++)
+            {
+                if (i == 0) mergedIntervals.Add(intervals[i]);
+                else {
+
+                    var previousInterval = mergedIntervals.Last();
+
+                    // check if it intervals have to be merge
+                    if (previousInterval.Item2 >= intervals[i].Item1)
+                    {
+                        var mergedInterval = new Tuple<int, int>(previousInterval.Item1, intervals[i].Item2);
+                        mergedIntervals[mergedIntervals.Count()-1] = mergedInterval;
+                    }
+                    else
+                    {
+                        mergedIntervals.Add(intervals[i]);
+                    }
+
+                }
+            }
+
+            return mergedIntervals;
         }
 
         public static List<Tuple<int, int>> ValidateAndParseIntervalsString(string intervalsString)
         {
-
-
             // remove whitespaces
             string intervalsStringNoSpaces = Regex.Replace(intervalsString, @"\s+", "");
 
@@ -30,7 +56,7 @@ namespace TSSCodingChallenge.IntervalMerge
             {
                 try
                 {
-                    // The following code is not optimized! There's a better way to do it
+                    // The following code is not optimized! There's a better way to do it!
 
                     // remove brace [
                     string intervalsStringNoLeft = Regex.Replace(intervalString.Value, @"\[+", "");
@@ -40,6 +66,9 @@ namespace TSSCodingChallenge.IntervalMerge
                     string[] namesArray = intervalsStringNoRight.Split(',');
 
                     int[] myInts = Array.ConvertAll(namesArray, s => int.Parse(s));
+
+                    // throw error if interval is inverted
+                    if(myInts[0]>myInts[1]) throw new Exception("No intervals found (Please check your input)");
 
                     var interval = new Tuple<int, int>(myInts[0], myInts[1]);
 
@@ -55,9 +84,6 @@ namespace TSSCodingChallenge.IntervalMerge
             {
                 throw new Exception("No intervals found (Please check your input)");
             }
-            Console.WriteLine(intervals.ToString());
-
-
 
             return intervals;
         }
